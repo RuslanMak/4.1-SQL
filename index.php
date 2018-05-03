56fc79a0734f44d73b25afe97e01ee14fbef0021
RuslanMak <makarovemd@gmail.com>
2018-05-04 01:01:18 +0300

    version-1

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+//http://university.netology.ru/u/rmakarov/4.1-SQL/index.php
+
 try {
     $pdo = new PDO('mysql:host=localhost;dbname=global', 'rmakarov', 'neto1741');
 } catch (PDOException $error) {
@@ -7,20 +9,8 @@ try {
     die();
 }
 
-//$pdo->exec('SET NAMES utf8');
-//
-//if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-//    $isbn = $_POST['isbn'];
-//    $name = $_POST['name'];
-//    $author = $_POST['author'];
-//    $sql = "SELECT * FROM books WHERE ((name LIKE :name) AND (isbn LIKE :isbn) AND (author LIKE :author))";
-//    $statement = $pdo->prepare($sql);
-//    $statement->execute(["name"=>"%{$name}%","isbn"=>"%{$isbn}%","author"=>"%{$author}%"]);
-//} else {
-//    $sql = "SELECT * FROM books";
-//    $statement = $pdo->prepare($sql);
-//    $statement->execute();
-//}
+$pdo->exec('SET NAMES utf8');
+
 
 $sql = "SELECT name FROM books";
 
@@ -30,4 +20,3 @@ foreach ($pdo->query($sql) as $row) {
 
 //echo "<pre>";
 //
-//print_r($_POST);
