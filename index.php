ed3944a2917a279e92777f4494164aed6318e973
RuslanMak <makarovemd@gmail.com>
2018-05-04 00:56:52 +0300

    vesion-1

--- index.php
+++ index.php
@@ -1,23 +1,33 @@
 <?php
 
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=netology_4.1', 'root', '');
+    $pdo = new PDO('mysql:host=localhost;dbname=global', 'rmakarov', 'neto1741');
 } catch (PDOException $error) {
     print "Error!: " . $error->getMessage() . "<br/>";
     die();
 }
 
-$pdo->exec('SET NAMES utf8');
+//$pdo->exec('SET NAMES utf8');
+//
+//if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+//    $isbn = $_POST['isbn'];
+//    $name = $_POST['name'];
+//    $author = $_POST['author'];
+//    $sql = "SELECT * FROM books WHERE ((name LIKE :name) AND (isbn LIKE :isbn) AND (author LIKE :author))";
+//    $statement = $pdo->prepare($sql);
+//    $statement->execute(["name"=>"%{$name}%","isbn"=>"%{$isbn}%","author"=>"%{$author}%"]);
+//} else {
+//    $sql = "SELECT * FROM books";
+//    $statement = $pdo->prepare($sql);
+//    $statement->execute();
+//}
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $isbn = $_POST['isbn'];
-    $name = $_POST['name'];
-    $author = $_POST['author'];
-    $sql = "SELECT * FROM books WHERE ((name LIKE :name) AND (isbn LIKE :isbn) AND (author LIKE :author))";
-    $statement = $pdo->prepare($sql);
-    $statement->execute(["name"=>"%{$name}%","isbn"=>"%{$isbn}%","author"=>"%{$author}%"]);
-} else {
-    $sql = "SELECT * FROM books";
-    $statement = $pdo->prepare($sql);
-    $statement->execute();
+$sql = "SELECT name FROM books";
+
+foreach ($pdo->query($sql) as $row) {
+    echo $row['name'] . "<br>";
 }
+
+//echo "<pre>";
+//
+//print_r($_POST);
